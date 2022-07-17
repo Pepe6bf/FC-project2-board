81aa58c48bfb53b54ca5e32f4b5f48affa5f7790
pepe6bf <pepe6bf@gmail.com>
2022-07-17 16:25:00 +0900

    #11 - jpa test 작성
    기본 CRUD + 연관 관계 매핑과 cascading이 잘 작동하는지 확인하기위해 작성,
    테스트 데이터가 없으면 실패함

---
+++ fc-project-board/src/test/java/com/fc/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,90 @@
+package com.fc.projectboard.repository;
+
+import com.fc.projectboard.config.JpaConfig;
+import com.fc.projectboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
+                             @Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void selectTest() throws Exception {
+        // Given
+
+        // When
+        List<Article> articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(979);
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void insertTest() throws Exception {
+        // Given
+        long previousCount = articleRepository.count();
+        Article newArticle = Article.of("new article", "new content", "#spring");
+
+        // When
+        Article savedArticle = articleRepository.save(newArticle);
+
+        // Then
+        assertThat(articleRepository.count())
+                .isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    void updateTest() throws Exception {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updateHashtag = "#springboot";
+        article.setHashtag(updateHashtag);
+
+        // When
+        Article savedArticle = articleRepository.saveAndFlush(article);
+
+        // Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updateHashtag);
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    void deleteTest() throws Exception {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+        // When
+        articleRepository.delete(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+    }
+}
